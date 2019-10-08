7441fa5ead98fefc2fc490bb77beeeec8b00a2af
Eathan Langford <eathan.langford@myob.com>
2019-10-08 12:02:06 +1100

    refactor win checking method and pass tests

--- src/com/company/Rules.java
+++ src/com/company/Rules.java
@@ -5,51 +5,47 @@ import java.util.Arrays;
 public class Rules {
 
     //maybe grab first element from passed coords, then check the next, if true move on, if not stop.
-    public static boolean checkHorizontals(Board board) {
-        Character[] oneRow = new Character[board.getBoard().length];
-        boolean horizontalWin;
-        for (int y = 0; y < board.getBoard().length; y++) {
-            for (int x = 0; x < board.getBoard()[0].length; x++) {
-                oneRow[x] = board.getPosition(x, y);
+
+    public static boolean checkLinearFromLastMove(Board board, int x, int y) {
+        char playerLastMove = board.getPosition(x, y);
+        int boardLength = board.getBoard().length;
+        int horizontalWinCounter = 0;
+        int verticalWinCounter = 0;
+
+        for (int i = 0; i < boardLength; i++) {
+            if (board.getPosition(i, y) == playerLastMove) {
+                horizontalWinCounter++;
+            }
+            if (board.getPosition(x, i) == playerLastMove) {
+                verticalWinCounter++;
             }
         }
-        horizontalWin = Arrays.stream(oneRow).distinct().count() == 1;
-        return horizontalWin;
+        return (horizontalWinCounter == boardLength) || (verticalWinCounter == boardLength) ;
     }
+    public static boolean checkDiagonalsFromLastMove(Board board, int x, int y) {
+        char playerLastMovePiece = board.getPosition(x, y);
+        int boardLength = board.getBoard().length;
+        int winCounterTopLeftToBottomRight = 0;
+        int winCounterTopRightToBottomLeft = 0;
 
-    public static boolean checkVerticals(Board board) {
-        boolean verticalWin;
-        Character[] oneColumn = new Character[board.getBoard()[0].length];
-        for (int x = 0; x < board.getBoard()[0].length; x++) {
-            oneColumn = board.getBoard()[x];
+        for (int i = 0; i < boardLength; i++) {
+            if (board.getPosition(i,i) == playerLastMovePiece){
+                winCounterTopLeftToBottomRight++;
+            }
+            if (board.getPosition(i, (-1*i+(boardLength-1))) == playerLastMovePiece){
+                winCounterTopRightToBottomLeft++;
+            }
         }
-        verticalWin = (Arrays.stream(oneColumn).distinct().count() == 1);
-        return verticalWin;
+     return (winCounterTopLeftToBottomRight == boardLength) || (winCounterTopRightToBottomLeft == boardLength);
     }
 
-    public static boolean checkDiagonalFromTopLeft(Board board) {
-        Character[] oneDiagnonal = new Character[board.getBoard().length];
-        boolean diagonalWin;
-        for (int x = 0; x < board.getBoard()[0].length; x++) {
-            oneDiagnonal[x] = board.getPosition(x, x);
+    public static boolean checkWinConditions(Board board, int x, int y) {
+
+        boolean diagonalsAvailableFromPiece = (x == y) || (y == (-1 * x + (board.getBoard().length-1)));
+
+        if (diagonalsAvailableFromPiece) {
+            return checkDiagonalsFromLastMove(board, x, y) || checkLinearFromLastMove(board, x, y);
         }
-        diagonalWin = Arrays.stream(oneDiagnonal).distinct().count() == 1;
-        return diagonalWin;
-    }
-
-    public static boolean checkDiagonalFromTopRight(Board board) {
-        Character[] oneDiagnonal = new Character[board.getBoard().length];
-        boolean diagonalWin;
-        int y = board.getBoard().length - 1;
-        for (int x = 0; x < board.getBoard().length; x++) {
-            oneDiagnonal[x] = board.getPosition(x, y);
-            y -= 1;
-        }
-        diagonalWin = Arrays.stream(oneDiagnonal).distinct().count() == 1;
-        return diagonalWin;
-    }
-
-    public static boolean checkWinConditions(Board board) {
-        return checkHorizontals(board) || checkVerticals(board) || checkDiagonalFromTopLeft(board) || checkDiagonalFromTopRight(board);
+        return checkLinearFromLastMove(board, x, y);
     }
 }
--- test/com/company/RulesTest.java
+++ test/com/company/RulesTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 
+import static com.company.Rules.*;
 import static org.junit.Assert.*;
 
 
@@ -14,7 +15,6 @@ public class RulesTest {
     Board gameBoard;
     Player player1;
     Player player2;
-    Board threeAcross;
     Board threeDiagonalTopLeft;
     Board threeDown;
     Board threeDiagonalTopRight;
@@ -26,49 +26,60 @@ public class RulesTest {
 
 
         gameBoard = new Board(3);
-
-        threeAcross = new Board(3);
-        threeDiagonalTopLeft = new Board(3);
-        threeDiagonalTopRight = new Board(3);
-        threeDown = new Board(3);
-        int inverseI =  2;
-
-        for (int i = 0; i < gameBoard.getBoard().length; i++) {
-            threeAcross.SetPieceOnBoard(player1,i,0);
-            threeDiagonalTopLeft.SetPieceOnBoard(player1,i,i);
-            threeDiagonalTopRight.SetPieceOnBoard(player1, i,inverseI);
-            threeDown.SetPieceOnBoard(player1,0,i);
-            inverseI-=1;
-        }
-
         newGame = new Game(3);
     }
 
     @After
     public void tearDown() {
-        threeAcross = null;
         threeDiagonalTopLeft = null;
         threeDown = null;
     }
 
     @Test
     public void shouldReturnWinConditionForThreeAcross() {
-        boolean actual = Rules.checkHorizontals(threeAcross);
+//            {'X','X','X'}
+//            {'*','*','*'}
+//            {'*','*','*'}
+        Board threeAcross = new Board(3);
+        threeAcross.SetPieceOnBoard(player1, 0, 0);
+        threeAcross.SetPieceOnBoard(player1, 1, 0);
+        threeAcross.SetPieceOnBoard(player1, 2, 0);
+        int[] lastMove = {1,0};
+
+        boolean actual = checkLinearFromLastMove(threeAcross, lastMove[0], lastMove[1]);
         boolean expected = true;
 
         Assert.assertEquals(expected, actual);
     }
     @Test
     public void shouldReturnWinConditionForThreeDown() {
-        boolean actual = Rules.checkVerticals(threeDown);
+//            {'X','*','*'}
+//            {'X','*','*'}
+//            {'X','*','*'}
+        Board threeDown = new Board(3);
+        threeDown.SetPieceOnBoard(player1, 0, 0);
+        threeDown.SetPieceOnBoard(player1, 0, 1);
+        threeDown.SetPieceOnBoard(player1, 0, 2);
+        int[] lastMove = {0,1};
+
+        boolean actual = checkLinearFromLastMove(threeDown, lastMove[0], lastMove[1]);
         boolean expected = true;
 
         Assert.assertEquals(expected, actual);
     }
-
+//
     @Test
     public void shouldReturnWinConditionForDiagonalLeftTopDown() {
-        boolean actual = Rules.checkDiagonalFromTopLeft(threeDiagonalTopLeft);
+//               {'X','*','*'}
+//               {'*','X','*'}
+//               {'*','*','X'}
+            Board threeDiagonalTopLeft = new Board(3);
+            threeDiagonalTopLeft.SetPieceOnBoard(player1, 0, 0);
+            threeDiagonalTopLeft.SetPieceOnBoard(player1, 1, 1);
+            threeDiagonalTopLeft.SetPieceOnBoard(player1, 2, 2);
+            int[] lastMove = {1,1};
+
+        boolean actual = checkDiagonalsFromLastMove(threeDiagonalTopLeft, lastMove[0], lastMove[1]);
         boolean expected = true;
 
         assertEquals(expected, actual);
@@ -76,15 +87,32 @@ public class RulesTest {
 
     @Test
     public void shouldReturnWinConditionForDiagonalRightTopDown() {
-        boolean actual = Rules.checkDiagonalFromTopRight(threeDiagonalTopRight);
+//               {'*','*','X'}
+//               {'*','X','*'}
+//               {'X','*','*'}
+        Board threeDiagonalTopRight = new Board(3);
+        threeDiagonalTopRight.SetPieceOnBoard(player1, 2, 0);
+        threeDiagonalTopRight.SetPieceOnBoard(player1, 1, 1);
+        threeDiagonalTopRight.SetPieceOnBoard(player1, 0, 2);
+        int[] lastMove = {0,2};
+
+        boolean actual = checkDiagonalsFromLastMove(threeDiagonalTopRight, lastMove[0], lastMove[1]);
         boolean expected = true;
 
         assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldReturnWinConditionForGeneralWinChecking() {
-        boolean actual = Rules.checkWinConditions(threeDown);
+    public void shouldReturnWinConditionForWinCheckingDiagonalBoard() {
+//                       {'*','*','X'}
+//                       {'*','X','*'}
+//                       {'X','*','*'}
+        Board threeDiagonalTopRight = new Board(3);
+        threeDiagonalTopRight.SetPieceOnBoard(player1, 2, 0);
+        threeDiagonalTopRight.SetPieceOnBoard(player1, 1, 1);
+        threeDiagonalTopRight.SetPieceOnBoard(player1, 0, 2);
+        int[] lastMove = {0,2};
+        boolean actual = checkWinConditions(threeDiagonalTopRight, lastMove[0], lastMove[1]);
         boolean expected = true;
 
         assertEquals(expected, actual);
