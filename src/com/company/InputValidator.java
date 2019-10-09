9f0b29cfeb2c004e98f0584508025040368a3eb0
Eathan Langford <eathan.langford@myob.com>
2019-10-10 10:42:25 +1100

    solve test for input validator to check for valid string format, add test to check numbers in string under board length

--- src/com/company/InputValidator.java
+++ src/com/company/InputValidator.java
@@ -2,7 +2,14 @@ package com.company;
 
 class InputValidator {
 
-    public static boolean isValid(String input){
+    public static boolean isValidFormatWithDigits(String input){
+        if(input.contains(",") && input.length() == 3){
+            return input.replace(",","").chars().allMatch(Character::isDigit);
+        }
+        return false;
+    }
+
+    public static boolean isValidInputWithinBoard(String input, Board board) {
         return true;
     }
 }
--- test/com/company/InputValidatorTest.java
+++ test/com/company/InputValidatorTest.java
@@ -1,16 +1,37 @@
 package com.company;
 
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InputValidatorTest {
+    private String userInputValidString;
+
+    @Before
+    public void setUp(){
+        userInputValidString = "2,0";
+    }
+
+    @After
+    public void tearDown(){
+        userInputValidString = null;
+    }
 
     @Test
-    public void shouldReturnTrueForValidString(){
-        boolean actual = InputValidator.isValid("2,0");
+    public void shouldReturnTrueForValidFormatString(){
+        boolean actual = InputValidator.isValidFormatWithDigits("2,0");
 
         Assert.assertTrue(actual);
     }
+
+    @Test
+    public void shouldReturnTrueForNumbersInStringWithinBoundsOfBoard() {
+        Board board = new Board(3);
+        boolean actual = InputValidator.isValidInputWithinBoard(userInputValidString, board);
+
+        Assert.assertTrue(actual);
+        }
 
 }
